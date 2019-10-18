9112834bbdf9e1ef72192ca24aad1a8f93f881ba
krlove <andrey.mischenko@speakap.nl>
2019-10-18 10:51:34 +0200

    Add closure to content script

--- content.js
+++ content.js
@@ -1,31 +1,33 @@
-document.addEventListener('DOMNodeInserted', function(event) {
-    const target = event.target;
+(function() {
+    document.addEventListener('DOMNodeInserted', function(event) {
+        const target = event.target;
 
-    if (target.className === 'ptm js-button-bar') {
-        const postButton = target.querySelector('button.action-submit');
-        const postAndUnsubscribeButton = postButton.cloneNode(true);
-        postAndUnsubscribeButton.innerHTML = 'Post and Unsubscribe';
-        postAndUnsubscribeButton.style.width = 'auto';
-        postAndUnsubscribeButton.style.marginRight = '2px';
-        postAndUnsubscribeButton.onclick = function() {
-            chrome.runtime.sendMessage({"startHandling": true}, function(response) {
-                if (response.submitted === true) {
-                    postButton.click();
+        if (target.className === 'ptm js-button-bar') {
+            const postButton = target.querySelector('button.action-submit');
+            const postAndUnsubscribeButton = postButton.cloneNode(true);
+            postAndUnsubscribeButton.innerHTML = 'Post and Unsubscribe';
+            postAndUnsubscribeButton.style.width = 'auto';
+            postAndUnsubscribeButton.style.marginRight = '2px';
+            postAndUnsubscribeButton.onclick = function() {
+                chrome.runtime.sendMessage({"startHandling": true}, function(response) {
+                    if (response.submitted === true) {
+                        postButton.click();
+                    }
+                });
+            };
+            postButton.parentNode.insertBefore(postAndUnsubscribeButton, postButton.nextSibling);
+
+            const observer = new MutationObserver(function(mutations) {
+                for (let i = 0; i < mutations.length; i++) {
+                    const mutation = mutations[i];
+                    if (mutation.attributeName === 'disabled') {
+                        postAndUnsubscribeButton.disabled = postButton.disabled;
+
+                        break;
+                    }
                 }
             });
-        };
-        postButton.parentNode.insertBefore(postAndUnsubscribeButton, postButton.nextSibling);
-
-        const observer = new MutationObserver(function(mutations) {
-            for (let i = 0; i < mutations.length; i++) {
-                const mutation = mutations[i];
-                if (mutation.attributeName === 'disabled') {
-                    postAndUnsubscribeButton.disabled = postButton.disabled;
-
-                    break;
-                }
-            }
-        });
-        observer.observe(postButton, {attributes: true});
-    }
-});
+            observer.observe(postButton, {attributes: true});
+        }
+    });
+})();
